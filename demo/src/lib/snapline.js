b58fd5f750937c0ac78c92b77f1d8148861e2f93
tfukaza <tfukaza@git.com>
2022-07-04 14:52:55 -0700

    Add new API to SnapLine class & fix background scroll

--- demo/src/lib/snapline.js
+++ demo/src/lib/snapline.js
@@ -581,6 +581,20 @@ class NodeUI extends Base {
             }
         }
     }
+    findInput(id) {
+        for (const o of this.inputs) {
+            if (o.gid === id)
+                return o;
+        }
+        return null;
+    }
+    findOutput(id) {
+        for (const o of this.outputs) {
+            if (o.gid === id)
+                return o;
+        }
+        return null;
+    }
     _initParseComponent(ui, content) {
         let u = null;
         switch (ui.type) {
@@ -614,10 +628,10 @@ class NodeUI extends Base {
         }
         return u;
     }
-    customMouseDown(e) {
-        console.debug(e);
+    customMouseDown(_) {
         this.pan_start_x = this.position_x;
         this.pan_start_y = this.position_y;
+        this.g.focusNode = this;
     }
     domMouseUp() {
         this.position_x = this.pan_start_x + this.g.dx / this.g.zoom;
@@ -764,6 +778,7 @@ class SnapLine {
             cameraWidth: 0,
             cameraHeight: 0,
             targetNode: null,
+            focusNode: null,
             hoverDOM: null,
             gid: 0,
             nodeArray: [],
@@ -787,12 +802,24 @@ class SnapLine {
         g.canvasContainer.appendChild(c);
         g.canvas = c;
         g.canvas.style.transform = `translate(${g.cameraWidth / 2}px, ${g.cameraHeight / 2}px)`;
+        g.canvasContainer.tabIndex = 0;
+        const bg = document.createElement('div');
+        bg.id = "sl-background";
+        bg.style.width = (g.cameraWidth * 10) + 'px';
+        bg.style.height = (g.cameraHeight * 10) + 'px';
+        bg.style.transform = `translate(${-g.cameraWidth * 5}px, ${-g.cameraHeight * 5}px)`;
+        bg.style.transformOrigin = "center";
+        bg.style.zIndex = "0";
+        bg.style.position = "absolute";
+        g.canvas.appendChild(bg);
+        g.canvasBackground = bg;
         g.canvasContainer.addEventListener('mousedown', function (e) {
             g.isMouseDown = true;
             g.mousedown_x = e.clientX;
             g.mousedown_y = e.clientY;
             g.camera_pan_start_x = g.camera_x;
             g.camera_pan_start_y = g.camera_y;
+            g.focusNode = null;
             for (const node of g.nodeArray) {
                 node.offFocus();
             }
@@ -809,7 +836,7 @@ class SnapLine {
                     g.camera_x = g.camera_pan_start_x - g.dx / g.zoom;
                     g.camera_y = g.camera_pan_start_y - g.dy / g.zoom;
                     g.canvas.style.transform = `matrix3d(${worldToCamera(g.camera_x, g.camera_y, g)})`;
-                    //g.canvasContainer.style.backgroundPosition = -g.camera_x* g.zoom + "px " + -g.camera_y* g.zoom + "px";
+                    g.canvasBackground.style.backgroundPosition = `${-g.camera_x}px ${-g.camera_y}px`;
                     g.canvas.style.cursor = "grabbing";
                 }
             }
@@ -836,7 +863,7 @@ class SnapLine {
             g.camera_y += d_zoom_y / g.zoom / g.zoom;
             g.canvas.style.transform = `matrix3d(${worldToCamera(g.camera_x, g.camera_y, g)})`;
             // scale background image
-            //g.canvasContainer.style.backgroundSize = g.zoom * 40 + "px " + g.zoom * 40 + "px";
+            //g.canvasBackground!.style.transform = `translate(${-g.cameraWidth*5}px, ${-g.cameraHeight*5}px) scale(${g.zoom}) `;
             e.preventDefault();
         });
         g.canvasContainer.addEventListener('mouseup', function (_) {
@@ -852,6 +879,16 @@ class SnapLine {
             g.dx_offset = 0;
             g.dy_offset = 0;
         });
+        g.canvasContainer.addEventListener('keydown', (e) => {
+            switch (e.key) {
+                case 'Delete':
+                    if (g.focusNode) {
+                        console.debug("Delete Node " + g.focusNode.gid);
+                        this.deleteNode(g.focusNode.gid);
+                    }
+                    break;
+            }
+        });
         console.info('Initialized SnapLine...');
         window.requestAnimationFrame(this.step.bind(this));
     }
@@ -864,13 +901,36 @@ class SnapLine {
     addNode(config, x, y) {
         const n = new NodeUI(config, this.g, x, y);
         this.g.globalNodes[n.gid] = n;
+        this.focusNode(n.gid);
         return n;
     }
     deleteNode(id) {
-        console.debug(id);
+        if (!(id in this.g.globalNodes))
+            return null;
+        const node = this.g.globalNodes[id];
+        this.g.canvas?.removeChild(node.dom);
+        delete this.g.globalNodes[id];
+        return id;
+    }
+    focusNode(id) {
+        if (!(id in this.g.globalNodes))
+            return null;
+        const node = this.g.globalNodes[id];
+        node.onFocus();
+        return id;
     }
     connectNodes(node0, outputID, node1, inputID) {
-        console.debug(node0, outputID, node1, inputID);
+        const n0 = this.g.globalNodes[node0];
+        const n1 = this.g.globalNodes[node1];
+        if (!n0 || !n1 || !(n0 instanceof NodeUI) || !(n1 instanceof NodeUI)) {
+            return null;
+        }
+        const o = n0.findOutput(outputID);
+        const i = n1.findInput(inputID);
+        if (!o || !i)
+            return null;
+        o.connectToInput(i);
+        return 0;
     }
 }
 
