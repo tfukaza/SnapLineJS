9fdca23b027877f566f81de49417dae6dbc92fa5
tfukaza <tfukaza@git.com>
2024-07-20 10:44:06 -0700

    Use Playwright for testing

---
+++ playwright.config.ts
@@ -0,0 +1,58 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const framework = process.env.FRAMEWORK || "vanilla";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests/e2e",
+
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Not working at the moment, need to figure out why. */
+  // webServer: {
+  //   command: `npm run server:${framework}`,
+  //   url: `http://localhost:3001`,
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
